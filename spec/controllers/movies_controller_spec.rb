a8d47ecfb337d95e4eed3b49173d6961f313cb62
Konrad Kierys <hello@konradkierys.com>
2012-06-02 18:18:06 +0100

    Wrote tests for Movies controller

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -7,6 +7,16 @@ describe MoviesController do
       get 'index'
       response.should be_success
     end
-  end
 
+    it "populates an array with movies" do
+      movie = FactoryGirl.create(:movie)
+      get 'index'
+      assigns(:movies).should eq([movie])
+    end
+
+    it "renders the 'index' view'" do
+      get :index
+      response.should render_template :index
+    end
+  end
 end
