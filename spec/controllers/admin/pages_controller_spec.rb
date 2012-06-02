ccbb93243985968624f6b405df6e5ffc614933f9
Konrad Kierys <hello@konradkierys.com>
2012-06-02 22:09:16 +0100

    Added tests for pages#add_movie action

--- spec/controllers/admin/pages_controller_spec.rb
+++ spec/controllers/admin/pages_controller_spec.rb
@@ -20,4 +20,15 @@ describe Admin::PagesController do
     end
   end
 
+  describe "GET 'add_movie'" do
+    it "returns http success" do
+      get :add_movie
+      response.should be_success
+    end
+
+    it "renders the :add_movie view" do
+      get :add_movie
+      response.should render_template :add_movie
+    end
+  end
 end
