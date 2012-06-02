77dee8d4eaa55cc2e9e3dd5b8eca86ddc5fd81b7
Konrad Kierys <hello@konradkierys.com>
2012-06-02 15:46:33 +0100

    Wrote validation to a model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ActiveRecord::Base
   attr_accessible :cast, :director, :duration, :genre, :rate, :release, :title
+
+  validates_presence_of :title, :release, :genre, :rate
+  validates :rate, :inclusion => { :in => 0..10 }
 end
