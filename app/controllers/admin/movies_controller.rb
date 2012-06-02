a537e74a203d015ede7aa766396454f060bc000e
Konrad Kierys <hello@konradkierys.com>
2012-06-03 00:42:52 +0100

    Made draft multipage form

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -9,4 +9,16 @@ class Admin::MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
+
+  def create
+    @movie = Movie.new(params[:movie])
+    @movie.current_step = session[:step]
+    if params[:back_button]
+      @movie.previous_step
+    else
+      @movie.next_step
+    end
+    session[:step] = @movie.current_step
+    render :new
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,32 @@
 class Movie < ActiveRecord::Base
   attr_accessible :cast, :director, :duration, :genre, :rate, :release, :title
+  attr_writer :current_step
 
   validates_presence_of :title, :release, :genre, :rate
   validates :rate, :inclusion => { :in => 0..10 }
+
+  def current_step
+    @current_step || steps.first
+  end
+
+  def steps
+    %w(add_movie possible_movies save_movie)
+  end
+
+  def next_step
+    self.current_step = steps[steps.index(current_step)+1]
+  end
+
+  def previous_step
+    self.current_step = steps[steps.index(current_step)-1]
+  end
+
+  def first_step?
+    current_step == steps.first
+  end
+
+  def last_step?
+    current_step == steps.last
+  end
+
 end
